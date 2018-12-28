1bac22d7c70f0086e723c525db9993e3adf3d782
xiewu <xiewu@cmcm.com>
2018-12-28 22:20:07 +0800

    浏览器通信遇到BUG

--- qrcode/js/qrcode_background.js
+++ qrcode/js/qrcode_background.js
@@ -1,11 +1,24 @@
 chrome.contextMenus.create({
   type: 'normal',
-  title: '生成二维码:',
+  title: '生成二维码',
   id: 'qrcodeCm',
   contexts: ['selection'],
   onclick: createQrcode
 });
 
 function createQrcode(info, tab) {
-  console.log('点击我的右键菜单了，哈哈哈!',info);
+  console.log('点击我的右键菜单了，哈哈哈!',info, tab);
+  sendMessageToContentScript(tab.id, info.selectionText, function(response){
+    console.log('来自content的回复：'+response);
+});
+}
+
+// 发送消息
+function sendMessageToContentScript(id, message, callback){
+
+  chrome.runtime.sendMessage(id+'', message, {includeTlsChannelId:true}, function(response){
+      if(callback) callback(response);
+  });
+      // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      // });
 }
